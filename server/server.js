0c425ed2aae7a1b95b36cae8e3893da9c74d3a58
Daniel Taschik <daniel@dubsmash.com>
2016-11-19 17:03:40 +0100

    introducing proper folder structure

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ var express = require('express')
 
 var app = express()
 
+app.get('/', function(req, res) {
+  res.json({notes: "Hello Erik!"})
+})
+
 app.get('/api/1/notes', function(req, res) {
   res.json({notes: "This is yorasdsdotebook. Edit this to start saving your notes!"})
 })
