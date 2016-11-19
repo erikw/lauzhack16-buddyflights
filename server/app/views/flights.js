dd4f368e3fdb83aa38a141be2fad7db493dbed14
Erik Westrup <erik.westrup@gmail.com>
2016-11-19 18:44:38 +0100

    xorigin fixed

--- server/app/views/flights.js
+++ server/app/views/flights.js
@@ -1,24 +1,28 @@
 function flights(req, res) {
-  // Allow cross origin
-  res.set('Access-Control-Allow-Origin', '*');
-
   var from = req.body.from;
   var to = req.body.to;
   var departure = req.body.departure;
-
-  if (from == null) {
-    res.status(400).send("'from' missing in request body.");
-  }
-  if (to == null) {
-    res.status(400).send("'to' missing in request body.");
-  }
-  if (departure == null) {
-    res.status(400).send("'departure' missing in request body.");
-  }
-
   console.log("Flights API valled with from=" + from + ", to=" + to + ", departure=" + departure);
 
-  res.json({result: "Hello flight!"});
+  // Allow cross origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+
+  if (from == null) {
+    res.status(400).send(JSON.stringify({error: "'from' missing in request body."}, null, 3));
+    return;
+  }
+  if (to == null) {
+    res.status(400).send(JSON.stringify({error: "'to' missing in request body."}, null, 3));
+    return;
+  }
+  if (departure == null) {
+    res.status(400).send(JSON.stringify({error: "'departure' missing in request body."}, null, 3));
+    return;
+  }
+
+
+  res.send(JSON.stringify({result: "Hello flight!"}, null, 3));
 }
 
 // Export
