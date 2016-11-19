46170d9a45696a7bf16e434d49fe4c2a23ea2e95
Erik Westrup <erik.westrup@gmail.com>
2016-11-19 16:54:50 +0100

    server running

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express()
 
-app.get('/', function(req, res) {
+app.get('/api/1/notes', function(req, res) {
   res.json({notes: "This is yorasdsdotebook. Edit this to start saving your notes!"})
 })
 
