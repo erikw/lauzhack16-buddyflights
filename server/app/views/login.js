e73963eff76474129f8a1e8db8893335aa83ffa9
Daniel Taschik <daniel@dubsmash.com>
2016-11-19 20:29:41 +0100

    added uniqueness check

--- server/app/views/login.js
+++ server/app/views/login.js
@@ -10,20 +10,41 @@ function login(req, res) {
   if (errors) {
     res.send(errors);
     return;
-  } else {  // store the object in the model
+  } else {
 
-    var newUser = User({facebookId: req.body.facebookId, city: req.body.city});
-    newUser.save(function (err) {
-      if (err)
-        re.send(err)
-      res.json({message: 'User created!'})
-    })
+    // no validation errors let's continue
+
+    var facebookId = req.body.facebookId;
+    var city = req.body.city;
+
+    // ensure uniqueness
+    var count = 0;
+    var query = User.find({facebookId: facebookId}).count();
+
+    query.exec(function (err, result) {
+      if (err) return handleError(err);
+      count = result;
+
+      if (count > 0) {
+        res.status(400);
+        res.send('User already existing!');
+        return;
+      } else {
+        // store the object in the DB
+        var newUser = User({facebookId: facebookId, city: city});
+        newUser.save(function (err) {
+          if (err)
+            re.send(err);
+          res.json({message: 'User created!'})
+        })
+      }
+    });
   }
 }
 
-function profile(req, res){
+function profile(req, res) {
   // Pull the user out of the DB
-  User.findById(req.params.facebookId, function(err, user) {
+  User.findById(req.params.facebookId, function (err, user) {
     if (err)
       res.send(err);
 
